0140abf998e9fcbbedd3825213aea08eecc90dc1
kuku-kun <melon813@naver.com>
2022-07-06 17:40:46 +0900

    js챌린지 3.1~2수업 정리

--- DAY03_1.js
+++ DAY03_1.js
@@ -17,9 +17,43 @@ document.title = "Hello! Broswer"
 // 요약하면 document는 Javascript에서 HTML로 접근가능한 방법
 
 // 2. HTML in Javascript
-const title = document.getElementById("title");
-console.log(title.id); 
-const hello = document.getElementsByClassName("hello");
-console.log(title.className);
+const title = document.getElementById('title');
+// console.dir(title);
+title.innerText = "Got you!"; // 표면적으로 html에 변화없이 js로 변화일으킴
+console.log(title.id); // title
+console.log(title.className); // hello
 
-title.innerText = "Study";
+// 3. element찾기(id, className)
+// getElementById() 안에 들어가는 건 html에서 id와 같은 이름이여야한다!
+// 보통은 id보다 className을 자주 사용하며, 동시 사용도 빈번
+// const bye = document.getElementById('bye');
+// bye.innerText = "Bye~";
+/* error: Cannot set properties of null (setting 'innerText')
+=> "'bye'라는 건 존재하지 않으니 bye 내부의 innerText를 호출하지 마십시오"
+*/
+const bye = document.getElementsByClassName('bye');
+console.log(bye);
+bye.innerText = "Bye~";
+
+// html 태그 자체를 가져오기
+const tag = document.getElementsByTagName("h3");
+console.log(tag);
+
+// 이상적인 Element 가져오기
+const query1 = document.querySelector('.hellp h2');
+// querySelector - element를 CSS방식으로 검색가능
+// => class 내부의 것들을 가져올 수 있다!
+// => 이를 통해 class를 먼저 찾안 뒤에 그 안의 것을 찾을 수 있다!
+console.log(query1); // 결과 : 첫번째 <h2>태그
+
+// div 안의 모든 태그를 가지고 오고 싶다면? -> querySelectorAll()
+const query2 = document.querySelectorAll('.hellp h2');
+console.log(query2); // List형식으로 json해서 가지고 온다
+
+// 참고
+const query3 = document.querySelector('#title');
+const query4 = document.getElementById('title');
+// 위는 동일하게 id로 설정한 태그를 부른다. 
+// 여기서 차이는 querySelector인 경우 CSS로 접근되어 아래의 태그를 접근가능하나,
+// getElementById는 아래 태그로 접근 불가능 
+console.log(query3);
